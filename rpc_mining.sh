724e380fc96cba08f37490e47d7c0114bb6d812d
vpsmining68 <vpsmining68@gmail.com>
2024-07-11 14:18:25 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe5pp6dyct7kapszarf7c -r community-pools.mysrv.cloud:10300 -m 5 -p rpc;
+    sleep 5;
+done
